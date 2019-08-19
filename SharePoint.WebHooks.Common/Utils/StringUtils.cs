c31dfa46aa84e8717bc1a1c06e2c89ab75240be6
David Vella <david@vella.uk.com>
2019-08-19 19:26:59 +0100

    Update

--- SharePoint.WebHooks.Common/Utils/StringUtils.cs
+++ SharePoint.WebHooks.Common/Utils/StringUtils.cs
@@ -8,13 +8,6 @@ namespace SharePoint.WebHooks.Common.Utils
     {
         public static IList<string> FormatTextToSentences(string text)
         {
-            // sanitize text a bit
-            text = Regex.Replace(text, @"[\r\n\t\f\v]", " ");
-            // remove extremely long words - they'll be headers, malformed parts or urls
-            text = Regex.Replace(text, @"\S{30,}", " ", RegexOptions.None);
-            // lastly, remove extra whitespace
-            text = Regex.Replace(text, @"( +)", " ");
-
             // figure out, which sentence length we're using based on set accuracylevel. The default value is 5120 (set by the API)
             const int splitStringLimit = 2560;
 
